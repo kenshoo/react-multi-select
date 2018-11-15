d5c89e80f4d95b037b189cce756aa331857c47cd
omerkenshoo <omer.hochman@kenshoo.com>
2018-11-15 15:14:27 +0200

    Add story with height percentage example

--- stories/multi-select.stories.js
+++ stories/multi-select.stories.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { action, storiesOf } from "@storybook/react";
 import ReactMultiSelect from "../src/components/multi_select";
-import { boolean, withKnobs } from "@storybook/addon-knobs";
+import { boolean, number, withKnobs } from "@storybook/addon-knobs";
+import Readme from "../README.md";
+import withReadme from "storybook-readme/with-readme";
 import customStyle from "./custom_style.scss";
 import SelectAll from "./custom_components/select_all";
 import SelectionStatus from "./custom_components/selection_status";
@@ -35,163 +37,213 @@ const withDisabledItems = generateItems(10).map((i, index) => ({
 
 storiesOf("React Multi Select", module)
   .addDecorator(withKnobs)
-  .add("Default view", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-      />
-    );
-  })
-  .add("Preselected Items", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        selectedItems={[{ id: 3, label: "Item 3" }]}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-      />
-    );
-  })
-  .add("With max selected items", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        maxSelectedItems={4}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-        messages={{ disabledItemsTooltip: "You can select up to 4 items" }}
-      />
-    );
-  })
-  .add("With some of the items disabled", () => {
-    return (
-      <ReactMultiSelect
-        items={withDisabledItems}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-        messages={{ disabledItemsTooltip: "You can select up to 4 items" }}
-      />
-    );
-  })
-  .add("With Custom Messages", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        messages={custom_messages}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-      />
-    );
-  })
-  .add("With Custom Styling", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        wrapperClassName={customStyle.wrapper}
-        listHeight={500}
-        selectedListHeight={540}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-        itemHeight={60}
-        selectAllHeight={40}
-      />
-    );
-  })
-  .add("Without Search and Select all", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        listHeight={500}
-        selectedListHeight={448}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={false}
-        showSelectAll={false}
-      />
-    );
-  })
-  .add("With Large Data (7000 items)", () => {
-    return (
-      <ReactMultiSelect
-        items={manyItems}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-      />
-    );
-  })
-  .add("Without Selected Items", () => {
-    return (
-      <ReactMultiSelect
-        items={manyItems}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-        showSelectedItems={boolean("Show selected items", false)}
-      />
-    );
-  })
-  .add("With custom components", () => {
-    return (
-      <ReactMultiSelect
-        items={items}
-        itemRenderer={Item}
-        selectAllRenderer={SelectAll}
-        searchRenderer={Search}
-        selectedItemRenderer={SelectedItem}
-        selectionStatusRenderer={SelectionStatus}
-        loading={boolean("Loading", false)}
-        onChange={action("onChange")}
-        showSearch={boolean("Show search", true)}
-        showSelectAll={boolean("Show select all", true)}
-      />
-    );
-  })
-  .add("With custom components and custom value", () => {
-    class ValueController extends React.Component {
-      state = {
-        value: ""
-      };
-
-      onChange = value => {
-        this.setState({ value });
-      };
-
-      render() {
-        return (
+  .add(
+    "Default view",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+        />
+      );
+    })
+  )
+  .add(
+    "With different height",
+    withReadme(Readme, () => {
+      return (
+        <div style={{ height: "1000px" }}>
           <ReactMultiSelect
-            searchValue={this.state.value}
-            searchValueChanged={this.onChange}
+            height={number("Height", 50, { min: 0, max: 100 }) + "%"}
             items={items}
-            itemRenderer={Item}
-            selectAllRenderer={SelectAll}
-            searchRenderer={SearchWithValue}
-            selectedItemRenderer={SelectedItem}
-            selectionStatusRenderer={SelectionStatus}
             loading={boolean("Loading", false)}
             onChange={action("onChange")}
             showSearch={boolean("Show search", true)}
             showSelectAll={boolean("Show select all", true)}
           />
-        );
-      }
-    }
+        </div>
+      );
+    })
+  )
+  .add(
+    "Preselected Items",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          selectedItems={[{ id: 3, label: "Item 3" }]}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+        />
+      );
+    })
+  )
+  .add(
+    "With max selected items",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          maxSelectedItems={4}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+          messages={{ disabledItemsTooltip: "You can select up to 4 items" }}
+        />
+      );
+    })
+  )
+  .add(
+    "With some of the items disabled",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={withDisabledItems}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+          messages={{ disabledItemsTooltip: "You can select up to 4 items" }}
+        />
+      );
+    })
+  )
+  .add(
+    "With Custom Messages",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          messages={custom_messages}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+        />
+      );
+    })
+  )
+  .add(
+    "With Custom Styling",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          wrapperClassName={customStyle.wrapper}
+          listHeight={500}
+          selectedListHeight={540}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+          itemHeight={60}
+          selectAllHeight={40}
+        />
+      );
+    })
+  )
+  .add(
+    "Without Search and Select all",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          listHeight={500}
+          selectedListHeight={448}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={false}
+          showSelectAll={false}
+        />
+      );
+    })
+  )
+  .add(
+    "With Large Data (7000 items)",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={manyItems}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+        />
+      );
+    })
+  )
+  .add(
+    "Without Selected Items",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={manyItems}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+          showSelectedItems={boolean("Show selected items", false)}
+        />
+      );
+    })
+  )
+  .add(
+    "With custom components",
+    withReadme(Readme, () => {
+      return (
+        <ReactMultiSelect
+          items={items}
+          itemRenderer={Item}
+          selectAllRenderer={SelectAll}
+          searchRenderer={Search}
+          selectedItemRenderer={SelectedItem}
+          selectionStatusRenderer={SelectionStatus}
+          loading={boolean("Loading", false)}
+          onChange={action("onChange")}
+          showSearch={boolean("Show search", true)}
+          showSelectAll={boolean("Show select all", true)}
+        />
+      );
+    })
+  )
+  .add(
+    "With custom components and custom value",
+    withReadme(Readme, () => {
+      class ValueController extends React.Component {
+        state = {
+          value: ""
+        };
 
-    return <ValueController />;
-  });
+        onChange = value => {
+          this.setState({ value });
+        };
+
+        render() {
+          return (
+            <ReactMultiSelect
+              searchValue={this.state.value}
+              searchValueChanged={this.onChange}
+              items={items}
+              itemRenderer={Item}
+              selectAllRenderer={SelectAll}
+              searchRenderer={SearchWithValue}
+              selectedItemRenderer={SelectedItem}
+              selectionStatusRenderer={SelectionStatus}
+              loading={boolean("Loading", false)}
+              onChange={action("onChange")}
+              showSearch={boolean("Show search", true)}
+              showSelectAll={boolean("Show select all", true)}
+            />
+          );
+        }
+      }
+
+      return <ValueController />;
+    })
+  );
