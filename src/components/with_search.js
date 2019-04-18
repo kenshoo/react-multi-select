c01a32f87e60bc5c473c7ca4a106acb0d78c21e8
Maksim <maksim.drobchak@kenshoo.com>
2019-04-18 14:40:39 +0300

    Create HOC for search

---
+++ src/components/with_search.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Search from "./search/search";
+import Column from "./column/column";
+export default Component => ({
+  searchRenderer = Search,
+  showSearch,
+  filterItems,
+  searchIcon,
+  searchValue,
+  messages,
+  ...others
+}) => {
+  const SearchRenderer = searchRenderer;
+  return showSearch ? (
+    <Column>
+      <SearchRenderer
+        onChange={filterItems}
+        searchIcon={searchIcon}
+        value={searchValue}
+        searchPlaceholder={messages.searchPlaceholder}
+      />
+      <Component {...others} />
+    </Column>
+  ) : (
+    <Column>
+      <Component {...others} />
+    </Column>
+  );
+};
